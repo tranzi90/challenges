a17ebcc2e0bfb94a59b98b4ba48c130dfae7e80e
denis_levchenko <tranzi90@gmail.com>
2022-10-23 23:47:11 +0600

    Initial commit

---
+++ isUnique.js
@@ -0,0 +1,10 @@
+// function isUnique(string) {
+//     return new Set(string).size === string.length
+// }
+
+const isUnique = (string) => new Set(string).size === string.length
+
+console.log(isUnique('abcdef')) // -> true
+console.log(isUnique('1234567')) // -> true
+console.log(isUnique('abcABC')) // -> true
+console.log(isUnique('abcadef')) // -> false
