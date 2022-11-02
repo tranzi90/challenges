cc576979f0467a58f1068bb8045a078e76e4ff78
denis_levchenko <tranzi90@gmail.com>
2022-11-03 04:19:20 +0600

    recreate custom Promise

--- myPromise.js
+++ myPromise.js
@@ -1,25 +1,42 @@
 function myPromise(executor) {
     let onResolve
+    let onReject
     let value
     let called = false
-    let fullfilled = false
+    let fulfilled = false
 
     this.then = function (fn) {
         onResolve = fn
 
-        if (fullfilled)
+        if (fulfilled)
             onResolve(value)
 
         called = true
         return this
     }
 
+    this.catch = function (fn) {
+        onReject = fn
+
+        if (!fulfilled)
+            onReject(value)
+
+        // called = true
+        return this
+    }
+
     function resolve(val) {
-        fullfilled = true
+        fulfilled = true
         value = val
 
         if (called)
             onResolve(val)
     }
-    executor(resolve)
+
+    function reject(val) {
+        value = val
+        onReject(val)
+    }
+
+    executor(resolve, reject)
 }
---
+++ parentheses.js
@@ -0,0 +1,28 @@
+function isBalanced(string) {
+    const start = '({['
+    const end = ']})'
+
+    const map = {
+        '}': '{',
+        ')': '(',
+        ']': '['
+    }
+
+    const queue = []
+
+    for (let i = 0; i < string.length; i++) {
+        const char = string[i]
+
+        if (start.includes(char)) {
+            queue.push(char)
+        } else if (end.includes(char)) {
+            const last = queue.pop()
+
+            if (map[char] !== last) {
+                return false
+            }
+        }
+    }
+
+    return !queue.length
+}
